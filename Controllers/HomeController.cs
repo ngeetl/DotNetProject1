939a9102d01e2969a2e594abd1d116c36bbe8335
yeni <ngeetl@naver.com>
2024-01-30 17:31:18 +0900

    불필요한 로직, 파일 삭제 및 전체적인 테마, UI수정

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,111 +9,111 @@ using System.Security.Claims;
 
 namespace DotNetProject1.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View(TicketModel.GetList());
+		}
 
-        public IActionResult TicketList()
-        {
-            return View(TicketModel.GetList());
-        }
+		//public IActionResult TicketList()
+		//{
+		//	return View(TicketModel.GetList());
+		//}
 
-        public IActionResult TicketChange([FromBody]TicketModel model)
-        {
-            model.Update();
+		public IActionResult TicketChange([FromBody] TicketModel model)
+		{
+			model.Update();
 
-            return Json(model); // ajax는 JSON 형태로 반환 받음
-            //return Redirect("/home/ticketlist");
-        }
-        public IActionResult BoardList(string search)
-        {
+			return Json(model); // ajax는 JSON 형태로 반환 받음
+								//return Redirect("/home/ticketlist");
+		}
+		public IActionResult BoardList(string search)
+		{
 
-            return View(BoardModel.GetList(search));
-        }
+			return View(BoardModel.GetList(search));
+		}
 
-        [Authorize]
+		[Authorize]
 		public IActionResult BoardWrite()
 		{
 			return View();
 		}
 
-        [Authorize]
-        public IActionResult BoardWrite_Input(string title, string contents)
-        {
-            var model = new BoardModel();
-
-            model.Title = title;   
-            model.Contents = contents;
-            model.Reg_user = Convert.ToUInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
-            model.Reg_username = User.Identity.Name;
-
-            model.Insert();
-
-            return Redirect("/home/boardlist");
-        }
-        public IActionResult BoardView(uint idx)
-        {
-
-            return View(BoardModel.Get(idx));
-        }
-
-        [Authorize]
-        public IActionResult BoardEdit(uint idx, string type)
+		[Authorize]
+		public IActionResult BoardWrite_Input(string title, string contents)
 		{
-            var model = BoardModel.Get(idx);
-            var userSeq = Convert.ToUInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+			var model = new BoardModel();
 
-            if (model.Reg_user != userSeq)
-            {
-                throw new Exception("수정할 수 있는 권한이 없습니다.");
-            }
+			model.Title = title;
+			model.Contents = contents;
+			model.Reg_user = Convert.ToUInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+			model.Reg_username = User.Identity.Name;
 
-            if (type == "update")
-            {
-                return View(model);
-            }
-            else if (type == "delete")
-            {
-                model.Delete();
-                return Redirect("/home/boardlist");
-            }
+			model.Insert();
 
-            throw new Exception("잘못된 요청입니다.");
-        }
+			return Redirect("/home/boardlist");
+		}
+		public IActionResult BoardView(uint idx)
+		{
 
-        [Authorize]
-        public IActionResult BoardEdit_Input(uint idx, string title, string contents)
-        {
-            var model = BoardModel.Get(idx);
-            var userSeq = Convert.ToUInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+			return View(BoardModel.Get(idx));
+		}
 
-            if (model.Reg_user != userSeq)
-            {
-                throw new Exception("수정할 수 있는 권한이 없습니다.");
-            }
+		[Authorize]
+		public IActionResult BoardEdit(uint idx, string type)
+		{
+			var model = BoardModel.Get(idx);
+			var userSeq = Convert.ToUInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
 
-            model.Title = title;
-            model.Contents = contents;
+			if (model.Reg_user != userSeq)
+			{
+				throw new Exception("수정할 수 있는 권한이 없습니다.");
+			}
 
-            model.Update();
+			if (type == "update")
+			{
+				return View(model);
+			}
+			else if (type == "delete")
+			{
+				model.Delete();
+				return Redirect("/home/boardlist");
+			}
 
-            return Redirect("/home/boardlist");
-        }
+			throw new Exception("잘못된 요청입니다.");
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[Authorize]
+		public IActionResult BoardEdit_Input(uint idx, string title, string contents)
+		{
+			var model = BoardModel.Get(idx);
+			var userSeq = Convert.ToUInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+			if (model.Reg_user != userSeq)
+			{
+				throw new Exception("수정할 수 있는 권한이 없습니다.");
+			}
+
+			model.Title = title;
+			model.Contents = contents;
+
+			model.Update();
+
+			return Redirect("/home/boardlist");
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
